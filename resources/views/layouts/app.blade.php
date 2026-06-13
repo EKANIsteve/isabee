c77b96c90020ea211ab678bc691b26430d972859
ONANA EKANI <ekani.durand@yahoo.com>
2026-06-13 14:07:34 +0200

    Force CSS reload in production

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,16 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     {{-- VITE --}}
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+@vite(['resources/css/app.css', 'resources/js/app.js'])
 
-    {{-- CSS PUBLIC --}}
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/style1.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/concours.css') }}">
+{{-- CSS PUBLIC AVEC VERSION POUR ÉVITER LE CACHE --}}
+<link rel="stylesheet" href="{{ asset('css/style.css') }}?v={{ time() }}">
+<link rel="stylesheet" href="{{ asset('css/style1.css') }}?v={{ time() }}">
+<link rel="stylesheet" href="{{ asset('css/concours.css') }}?v={{ time() }}">
 
-    {{-- FONT AWESOME --}}
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
-
+{{-- FONT AWESOME --}}
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
     @stack('styles')
 </head>
 
